2cbf021c9eca5ea2ccf88c9086c7e9f67b95d220
enricorenna01 <erenna39@gmail.com>
2024-11-23 09:48:26 +0100

    feat: adjusted html and javascript

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,76 @@
 //DICHIARO LE VARIABILI
 
+//Prendo il form dal dom
 
-//Definisco una variabile kmNumber e la trasformo da stringa a numero
-const kmNumber = Number(prompt("Inserisci il numero dei km che vuoi percorrere"));
-console.log(kmNumber);
+const myForm = document.getElementById("calculator-form");
 
-//Definisco una variabile userAge e la trasformo da stringa a numero
-const userAge = Number(prompt("Inserisci la tua età"));
-console.log(userAge);
+//Campi del form
 
-//Salvo una variabile kmPrice che sarebbe il prezzo al km
-const kmPrice = 0.21;
-
-//Salvo una variabile underAge per determinare il valore dell'età di un minorenne
-const underAge = 18;
-
-//Salvo una variabile overAge per determinare il valore dell'età dai 65 anni in sù
-const overAge = 65;
-
-//Salvo una variabile underDiscount per determinare il valore sconto dei minorenni
-const underDiscount = 20;
-
-//Salvo una variabile underDiscount per determinare il valore sconto dei over 65
-const overDiscount = 40;
+const kmNumber = document.getElementById("km-number");
+const userAge = document.getElementById("user-age");
+const resultContainer = document.getElementById("result-container");
+const resultText = document.getElementById("result");
 
 
-//CALCOLO DEL BIGLIETTO
+//LEGGO I DATI INSERITI DALL'UTENTE
+
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    console.log(kmNumber.value);
+    console.log(userAge.value);
+
+    //Salvo una variabile kmPrice che sarebbe il prezzo al km
+
+    const kmPrice = 0.21;
+
+    //Salvo una variabile underAge per determinare il valore dell'età di un minorenne
+
+    const underAge = 18;
+
+    //Salvo una variabile overAge per determinare il valore dell'età dai 65 anni in sù
+
+    const overAge = 65;
+
+    //Salvo una variabile underDiscount per determinare il valore sconto dei minorenni
+
+    const underDiscount = 20;
+
+    //Salvo una variabile underDiscount per determinare il valore sconto dei over 65
+
+    const overDiscount = 40;
 
 
-//Salvo una variabile price per determinare il prezzo
-let price = kmNumber * kmPrice;
+    //CALCOLO DEL BIGLIETTO
 
-//SE utente minorenna applico sconto underAge
-if (userAge < underAge) {
+
+    //Salvo una variabile price per determinare il prezzo
+
+    let price = kmNumber.value * kmPrice;
+    console.log(price)
+    
+    //SE utente minorenna applico sconto underAge
+
+    if (userAge < underAge) {
     price = price - ((price * underDiscount) / 100);
 
-//ALTRIMENTI SE utente over 65 sconto overAge
-} else if (userAge > overAge) {
+    //ALTRIMENTI SE utente over 65 sconto overAge
+
+    } else if (userAge > overAge) {
     price = price - ((price * overDiscount) / 100);
 
-}
+    };
 
 
-//OUTPUT
+    //OUTPUT
 
-//Restituisco il risultato
-const result = `Il prezzo è ${price.toFixed(2)}€`;
-console.log(result);
+    resultText.innerHTML = `Il costo del biglietto è ${price}€`;
+    resultContainer.classList.remove("d-none");
+
+
+    //SVUOTO I CAMPI
+    kmNumber.value = "";
+    userAge.value = "";
+
+
+    });
